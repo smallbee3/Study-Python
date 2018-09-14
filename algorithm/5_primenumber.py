6eaebe9f9a7c3ac4a2953c47488339d6304991bd
smallbee <smallbee3@gmail.com>
2018-09-14 18:22:34 +0900

    Algorithm: 5_PRACTICE 5-5, 5-6, 5-7 solved

--- algorithm/5_primenumber.py
+++ algorithm/5_primenumber.py
@@ -42,3 +42,51 @@ def prime_list(num):
 #
 #
 # print(prime_nth(52))
+
+
+# p.125
+
+HASHTABLE_SIZE = 10
+
+
+def alphabet_to_prime(str):
+    list52 = prime_list(52)
+
+    # Alphabet - primenumber Hash table 만들기
+    hash_table = {}
+    for i, j in enumerate(['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']):
+        hash_table[j] = list52[i]
+    sum = 0
+    for i in str:
+        sum += hash_table[i]
+    return sum % HASHTABLE_SIZE
+
+
+# 5-5
+print('5-5')
+print(alphabet_to_prime('Esther'))
+print(alphabet_to_prime('Ben'))
+print(alphabet_to_prime('Bob'))
+print(alphabet_to_prime('Dan'))
+
+
+# 5-6
+print('5-6')
+print(alphabet_to_prime('A'))
+print(alphabet_to_prime('AA'))
+print(alphabet_to_prime('AAA'))
+print(alphabet_to_prime('AAAA'))
+
+
+# 5-7
+print('5-7')
+print(alphabet_to_prime('MAUS'))
+print(alphabet_to_prime('FunHome'))
+print(alphabet_to_prime('Watchmen'))
+
+
+# My Test
+print('My Test')
+print(alphabet_to_prime('MAUS'))
+print(alphabet_to_prime('AUSM'))
+print(alphabet_to_prime('SUAM'))
