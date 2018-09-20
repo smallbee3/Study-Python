6e9504e2aab87d3cc27da76277a776fb07a9b600
smallbee <smallbee3@gmail.com>
2018-09-20 17:35:18 +0900

    Algorithm: 6_bread-first-search book answer added

--- algorithm/6_breadth_first_search.py
+++ algorithm/6_breadth_first_search.py
@@ -28,3 +28,26 @@ q = queue.Queue()
 for person in graph['you']:
     q.put(person)
 func(q)
+
+
+# Book answer
+
+def person_is_seller(name):
+    return name[-1] == 'y'
+
+
+def fun2(search_queue):
+    while search_queue:
+        person = search_queue.popleft()
+        if person_is_seller(person):
+            print(f'{person} is a mango seller!')
+            return True
+        else:
+            search_queue += graph[person]
+    return False
+
+
+from collections import deque
+search_queue = deque()
+search_queue += graph['you']
+fun2(search_queue)
