4d7924e0148c66382a598c21004b20ab9986d855
smallbee <smallbee3@gmail.com>
2019-01-16 14:12:59 +0900

    generator| code modified and the new fact learned : for loop doesn't operate if generator is already iterated

--- python/generator.py
+++ python/generator.py
@@ -13,7 +13,7 @@ def square_numbers(nums):
 
 my_nums = square_numbers([1, 2, 3, 4, 5])
 
-print(my_nums)
+# print(my_nums)
 
 
 # 1) next() 함수를 통해 리스트 값 출력
@@ -23,12 +23,24 @@ print(my_nums)
 # print(next(my_nums))
 # print(next(my_nums))
 
+# 1-2) __next__() magic method를 통해 리스트 값 출력
+print(my_nums.__next__())
+print(my_nums.__next__())
+print(my_nums.__next__())
+print(my_nums.__next__())
+print(my_nums.__next__())
+
 
 # 2) StopIteration 예외가 발생
 # print(next(my_nums))
+# print(my_nums.__next__())
 
 
 # 3) 제너레이터는 일반적으로 for 루프를 통해서 호출하여 사용
+
+# 190116
+# for loop below doesn't work because all generator are called already.
+
 for i in my_nums:
     # print(next(i))
     print(i)
